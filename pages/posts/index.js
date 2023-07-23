a541caa279f86427066de6c44659deef98ce85a9
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-07-23 13:19:16 +0200

    added posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,24 @@
+import { index } from '@/actions/postsApi';
+import Card from '@/components/Card';
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
 
-const Posts = () => {
+const Posts = ({posts}) => {
+  const generatePosts = () => {
+		return posts?.map(post => {
+			return (
+				<Col sm={12} md={4} lg={4}>
+					<Card post={post} />
+				</Col>
+			)
+		})
+	}
   return (
-    <div>Posts</div>
+    <Container style={{paddingTop: 150}}>
+			<Row>
+				{generatePosts()}
+			</Row>
+		</Container>
   )
 }
 
